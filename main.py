dc6bd826dcc761c085ba75eaf4a4569d1a31e4fe
Thomas Harrison <twh2898@vt.edu>
2023-03-14 20:15:17 -0400

    Split data files on date change

--- main.py
+++ main.py
@@ -18,45 +18,67 @@ BAUD = 9600
 OUTDIR = 'data'
 
 os.makedirs(OUTDIR, exist_ok=True)
-start = datetime.now()
-data_file = '{}.csv'.format(start.strftime('%Y%m%d_%H%M%S'))
 
 
 def read_data(s, f):
-    l.info('Reading data')
+    last_date = datetime.now().date()
 
-    data = s.readline()
-    l.debug('Data is %s', data)
+    while True:
+        l.info('Reading data')
 
-    line = data.decode().strip()
-    now = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-    line = str(now) + ',' + line
-    print(line)
-    n = f.write(line + '\n')
-    l.debug('Write return %d', n)
-    f.flush()
+        data = s.readline()
+        l.debug('Data is %s', data)
+
+        line = data.decode().strip()
+
+        now = datetime.now()
+        if now.date() != last_date:
+            # Yes, I know this drops one point... sue me
+            l.info('Date changed %s to %s, Switching', now.date(), last_date)
+            return
+        last_date = now.date()
+
+        now_str = now.strftime('%Y-%m-%dT%H:%M:%S')
+        line = now_str + ',' + line
+        print(line)
+        n = f.write(line + '\n')
+        l.debug('Write return %d', n)
+        f.flush()
+
+        l.debug('loop')
 
 
 def main():
-    l.info('Writing data to %s', data_file)
-    l.info('Collecting data from %s at baud %d', PORT, BAUD)
+    l.info('Connecting to %s at baud %d', PORT, BAUD)
     try:
-        with serial.Serial(PORT, BAUD) as s, \
-                open(os.path.join(OUTDIR, data_file), 'w') as f:
-
+        with serial.Serial(PORT, BAUD) as s:
             l.debug('Serial port open')
+
             while True:
-                read_data(s, f)
+                start = datetime.now()
+                data_file = '{}.csv'.format(start.strftime('%Y%m%d'))
+                l.info('Writing data to %s', data_file)
+
+                with open(os.path.join(OUTDIR, data_file), 'a') as f:
+                    read_data(s, f)
 
     except SerialException:
         l.exception('Error reading data')
+
+    l.info('Exiting main')
 
 
 if __name__ == '__main__':
     try:
         while True:
             main()
+            l.info('Waiting for a new connection')
             sleep(1)
+
     except RuntimeError as e:
         l.exception('Runtime error')
+        raise e
+
+    except Exception as e:
+        l.exception('Exception')
         raise e

f579dd8b382edfd8e1b22838aa640eeb2446aa95
Thomas Harrison <twh2898@vt.edu>
2023-03-14 20:15:23 -0400

    Update scripts

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 scp main.py pi@raspberrypi:~/
 scp collect_data.service pi@raspberrypi:~/.config/systemd/user/
 ssh pi@raspberrypi "systemctl --user daemon-reload"
-# ssh pi@raspberrypi "systemctl --user restart collect_data.service"
+ssh pi@raspberrypi "systemctl --user restart collect_data.service"

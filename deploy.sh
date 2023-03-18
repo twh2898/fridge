dfacc1c20fe3677e17088c8dde77cdd678102096
Thomas Harrison <twh2898@vt.edu>
2023-03-18 00:51:56 -0400

    Remove deploy.sh, update deploy.yaml for new root

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-scp main.py pi@raspberrypi:~/
-scp collect_data.service pi@raspberrypi:~/.config/systemd/user/
-ssh pi@raspberrypi "systemctl --user daemon-reload"
-ssh pi@raspberrypi "systemctl --user restart collect_data.service"

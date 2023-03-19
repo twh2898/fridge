d0c8826fdba37262986f6e06077a692fa3eb0aea
Thomas Harrison <twh2898@vt.edu>
2023-03-19 14:39:53 -0400

    Document bench.py, add time function

--- analyze/bench.py
+++ analyze/bench.py
@@ -1,3 +1,4 @@
+"""Helper functions for benchmark and timing."""
 
 from time import process_time
 
@@ -5,9 +6,26 @@ _timers: dict[str, float] = {}
 
 
 def start(name: str):
+    """Store the current time for name."""
     _timers[name] = process_time()
 
 
 def end(name: str):
+    """Get the time delta for name."""
     assert name in _timers, 'No timer {} exists'.format(name)
     return process_time() - _timers[name]
+
+
+def time(fn):
+    """Time a function."""
+    name = f'{fn.__module__}.{fn.__name__}'
+
+    def wrapper(*a, **kw):
+        start = process_time()
+        res = fn(*a, **kw)
+        end = process_time()
+        delta = end - start
+        print(name, 'in', delta, 's')
+        return res
+
+    return wrapper
